79ceadc8fa3cc9c3f2036442afdef7f348843753
Santhosh.V <santhosh.v4@cognizant.com>
2023-12-03 17:23:52 +0530

    Episode 3 - part 2 code

--- App.js
+++ App.js
@@ -1,37 +1,23 @@
-/*
-React.createElement is an object
-once root.render is run it converts heading to HTML in browser
-
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*Single Element*/
-const heading = React.createElement(
-  "h1",
-  { id: "heading", class: "heading" },
-  "Hello world from React"
+const title = "Testing new title";
+
+/* Another way of functional component */
+const HeadingComponent2 = () => <h1>My second header</h1>;
+
+const HeadingComponent3 = () => {
+  return <h1>Another header with return</h1>;
+};
+
+/* Functional component */
+const HeadingComponent = () => (
+  <div>
+    <HeadingComponent2 />
+    <HeadingComponent3 />
+    <h1>New header for react component - {title}</h1>
+  </div>
 );
 
-/*Nested Elements*/
-const parent = React.createElement(
-  "div",
-  { class: "parent" },
-  React.createElement(
-    "div",
-    { class: "child" },
-    React.createElement("h1", { class: "header" }, "My H1 tag")
-  )
-);
-/*Nested Elements with siblings*/
-const parentWithSibling = React.createElement(
-  "div",
-  { class: "parent" },
-  React.createElement("div", { class: "child" }, [
-    React.createElement("h1", { class: "header" }, "My H1 tag"),
-    React.createElement("h2", { class: "header2" }, "My H2 tag"),
-  ])
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parentWithSibling);
+root.render(<HeadingComponent />);
